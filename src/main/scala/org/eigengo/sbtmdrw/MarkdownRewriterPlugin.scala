8a3abdfce79262cb6a93c5593d3ac99e135659e3
Jan Machacek <jan.machacek@gmail.com>
2013-07-31 16:21:34 +0100

    Renderer fallback

--- src/main/scala/org/eigengo/sbtmdrw/MarkdownRewriterPlugin.scala
+++ src/main/scala/org/eigengo/sbtmdrw/MarkdownRewriterPlugin.scala
@@ -2,8 +2,11 @@ package org.eigengo.sbtmdrw
 
 import sbt._
 import sbt.complete.Parsers._
-import java.io.{File, FileWriter}
+import java.io.{BufferedWriter, FileOutputStream, File, FileWriter}
 import sbt.Load.BuildStructure
+import java.rmi.activation.Activator
+import org.eigengo.sbtmdrw.renderers.{WordpressMarkdownRenderer, ActivatorMarkdownRenderer}
+import scala.io.Source
 
 /**
  * Plugin for SBT that provides a task to convert Markdown files into other text-based formats, in the first instance,
@@ -15,12 +18,19 @@ object MarkdownRewriterPlugin extends Plugin {
     Keys.commands += mdrwCommand
   )
 
+  def renderers: PartialFunction[Option[String], MarkdownRenderer] = {
+    case Some("activator") => ActivatorMarkdownRenderer()
+    case Some("wordpress") => WordpressMarkdownRenderer()
+
+    case _                 => ActivatorMarkdownRenderer() // TODO: some sensible default
+  }
+
   private val args = (Space ~> StringBasic).*
 
   private lazy val mdrwCommand = Command("mdrw")(_ => args)(doCommand)
 
   def doCommand(state: State, args: Seq[String]): State = {
-    val templateType = args.headOption.getOrElse("activator")
+    val renderer = renderers(args.headOption)
 
     val extracted = Project.extract(state)
     val buildStruct = extracted.structure
@@ -47,7 +57,7 @@ object MarkdownRewriterPlugin extends Plugin {
     }
 
     projectList.foreach {
-      case (_, project) => processProject(state, project, buildUnit.localBase, templateType)
+      case (_, project) => processProject(state, project, buildUnit.localBase, renderer)
     }
 
     state
@@ -62,7 +72,7 @@ object MarkdownRewriterPlugin extends Plugin {
     }
   }
 
-  private def processProject(state: State, project: ResolvedProject, projectRoot: File, templateType: String): Unit = {
+  private def processProject(state: State, project: ResolvedProject, projectRoot: File, renderer: MarkdownRenderer): Unit = {
     val tutorialBase = project.base / "tutorial"
     val tutorialTarget = projectRoot / "tutorial"
     state.log.info("Processing markdown files in " + tutorialBase + " to " + tutorialTarget)
@@ -70,6 +80,20 @@ object MarkdownRewriterPlugin extends Plugin {
     val markdownFiles = tutorialBase.listFiles(new FileFilter {
       def accept(pathname: File): Boolean = isMarkdownFile(pathname)
     })
+
+    markdownFiles.foreach(processMarkdownFile(state, renderer, tutorialTarget))
+  }
+
+  private def processMarkdownFile(state: State, renderer: MarkdownRenderer, targetDirectory: File)(file: File): Unit = {
+    state.log.info("Converting file " + file)
+
+    new MarkdownRewriter(file, renderer).run() match {
+      case Left(error)    => state.log.error(error)
+      case Right(content) =>
+        val w = new BufferedWriter(new FileWriter(targetDirectory / file.base + ".html"))
+        w.write(content)
+        w.close()
+    }
   }
 
 }
